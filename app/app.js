95a6499e79a4ed9f3e0d969fafc8973f669b5ac0
Kartikey Sharma <kartikey.sharma@infomatrixinc.com>
2016-10-28 17:35:07 +0530

    update for bug fixing

--- app/app.js
+++ app/app.js
@@ -171,7 +171,14 @@ oTech.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$htt
   	 url: "/dashboard/RadioPerformance",
      templateUrl: "app/views/RadioPerformanceReport.html",
      controller: 'ReportsTableauController'
-}) 
+}).state("droolRuleTemplete", {
+    url: "/dashboard/droolruleTemplete",
+    templateUrl: "app/views/create_drools_rule.html",
+    controller: 'DroolruleTempleteController',
+	ncyBreadcrumb: {
+        label: 'Test Plan'
+    }
+})
                 
                 .state("myDevices", {
                     url: "/dashboard/myDevices",
@@ -1010,6 +1017,80 @@ oTech.directive('numbersOnly', function () {
             }
         };
     });
+
+angular.module('ui.grid').factory('InlineEdit', ['$interval', '$rootScope', 'uiGridRowEditService',
+                                                 function ($interval, $rootScope, uiGridRowEditService) {
+                                                     function inlineEdit(entity, index, grid) {
+                                                         this.grid = grid;
+                                                         this.index = index;
+                                                         this.entity = {};
+                                                         this.isEditModeOn = false;
+                                                         this.init(entity);
+                                                     }
+
+                                                     inlineEdit.prototype = {
+                                                         init: function (rawEntity) {
+                                                             var self = this;
+
+                                                             for (var prop in rawEntity) {
+                                                                 self.entity[prop] = {
+                                                                     value: rawEntity[prop],
+                                                                     isValueChanged: false,
+                                                                     isSave: false,
+                                                                     isCancel: false,
+                                                                     isEdit: false
+                                                                 }
+                                                             }
+                                                         },
+
+                                                         enterEditMode: function (event) {
+                                                             event && event.stopPropagation();
+                                                             var self = this;
+                                                             self.isEditModeOn = true;
+
+                                                             // cancel all rows which are in edit mode
+                                                             self.grid.rows.forEach(function (row) {
+                                                                 if (row.inlineEdit && row.inlineEdit.isEditModeOn && row.uid !== self.grid.rows[self.index].uid) {
+                                                                     row.inlineEdit.cancelEdit();
+                                                                 }
+                                                             });
+
+                                                             // Reset all the values
+                                                             for (var prop in self.entity) {
+                                                                 self.entity[prop].isSave = false;
+                                                                 self.entity[prop].isCancel = false;
+                                                                 self.entity[prop].isEdit = true;
+                                                             }
+                                                         },
+
+                                                         saveEdit: function (event) {
+                                                             event && event.stopPropagation();
+                                                             var self = this;
+
+                                                             self.isEditModeOn = false;
+
+                                                             for (var prop in self.entity) {
+                                                                 self.entity[prop].isSave = true;
+                                                                 self.entity[prop].isEdit = false;
+                                                             }
+
+                                                             uiGridRowEditService.saveRow(self.grid, self.grid.rows[self.index])();
+                                                         },
+
+                                                         cancelEdit: function (event) {
+                                                             event && event.stopPropagation();
+                                                             var self = this;
+
+                                                             self.isEditModeOn = false;
+                                                             for (var prop in self.entity) {
+                                                                 self.entity[prop].isCancel = true;
+                                                                 self.entity[prop].isEdit = false;
+                                                             }
+                                                         }
+                                                     }
+
+                                                     return inlineEdit;
+                                                 }]);
 
 
 
