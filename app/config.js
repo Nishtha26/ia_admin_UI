60b322d3f194371469bab5ed2fad9af0d55196aa
LAPTOP-4R6ICKKF\Vijay <vijayvittal.yd@gmail.com>
2018-09-17 20:56:50 +0530

    Merged from latest master copy

--- app/config.js
+++ app/config.js
@@ -132,11 +132,11 @@ oApp.config = {
             type: "Network Performance"
         }
     ],
-    BASE_URL: 'http://localhost:8081/IAPORTAL/',
+    BASE_URL: 'http://test.orchestratec.net:8080/IAPORTAL/',
     MEASUREMENT_URL: 'http://test.orchestratec.net:8080/IAPORTAL/rest/measurement/',
     REPORT_HOST_URL: 'http://iareport.orchestratec.net/',
     REPORT_NAME: 'ConsolidatedKPI5_13/HomePage',
-    DASHBOARD_REPORTING_BASE_URL: 'http://localhost:8081/IAPORTAL/',
+    DASHBOARD_REPORTING_BASE_URL: 'http://test.orchestratec.net:8080/IAPORTAL/',
     jobListGridOptions: {
         paginationPageSizes: [1, 25, 50, 75],
         paginationPageSize: 25,
